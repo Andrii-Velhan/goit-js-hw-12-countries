58c9b49322dfc061ac367a4a171803b3abc5d121
Andrii-Velhan <velganaa@gmail.com>
2020-11-15 21:37:56 +0200

    this working!!!)

--- src/index.js
+++ src/index.js
@@ -1,38 +1,72 @@
 import './styles.css';
 import countryCardTpl from './templates/country-card.hbs'
-//import fetchCountries from './js/fetchCountries';
+import countryListTpl from './templates/list-card.hbs'
+import API from './js/fetchCountries';
+import getRefs from './js/get-refs';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+import { error } from '@pnotify/core';
 
-//fetchCountries(ukraine);
-const refs = {
-    cardContainer: document.querySelector('.js-card-container'),
-    searchInput: document.querySelector('input#inputName'),
-};
+const debounce = require('lodash.debounce');
+const refs = getRefs();
 
-refs.searchInput.addEventListener('input', onSearch)
+refs.searchInput.addEventListener('input', debounce(onSearch, 500))
 
 function onSearch (e) {
     e.preventDefault();
 
     const input = e.target;
     const searchQuery = input.value;
-    // const input = e.target;
-    // const searchQuery = input.value;
+    if (searchQuery.length === 0) {
+        clearResult();
+        return
+    }
+    
     console.log(searchQuery);
 
-    fetchCountries(searchQuery)
-    .then(renderCountryCard)
-    .catch(error => console.log(error))
-    .finally(() => input.reset())
-}
-
-function fetchCountries(searchQuery) {
-    return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`).then(
-        response => {
-            return response.json();
-    });  
+    API.fetchCountries(searchQuery)
+        .then(renderCountryCard)
+        .catch(onFetchError)        
 }
 
 function renderCountryCard(country) {
-        const markUp = countryCardTpl(country);        
+    const markUp = countryCardTpl(country);        
+    const listMarkUp = countryListTpl(country);
+
+    if (country.status === 404) {
+        clearResult();
+        return error({
+            text: 'The country for your request was not found. Please try again',
+            type: 'info',
+            delay: 2000
+            // delay: refs.searchInput.addEventListener('input', 2000),
+        });        
+    }
+    if (country.length > 10) {
+        clearResult();
+        console.log(`country.length: ${country.length}`);
+        return error({
+            text: `Many countries found, please make your request more specific !!!`,
+            type: 'info',
+            delay: 2000
+            // delay: refs.searchInput.addEventListener('input', 2000),
+        });
+        
+    }
+    if (country.length >=2 && country.length <= 10) {
+        console.log(`country.length: ${country.length}`);
+        return refs.cardContainer.innerHTML = listMarkUp;
+    }    
         refs.cardContainer.innerHTML = markUp;
 }
+
+function onFetchError(error) {
+    console.log('Это CATCH!!!');            
+    console.dir(error);
+    alert('Упс, что-то пошло не так');
+}
+
+function clearResult() {
+    refs.cardContainer.innerHTML = "";
+}
+
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,16 +1,9 @@
-// import { reject } from "core-js/fn/promise";
-
-// export default  fetchCountries = () => {
-//     fetch('https://restcountries.eu/rest/v2/name/ukr')
-//     .then(response => {
-//         ///console.log(response.json());
-//         return response.json();
-//     })
-//     .then(searchQuery => {
-//         console.log(searchQuery);
-//     })
-//     .catch(error => {
-//         console.log(error);
-//     });
+const BASE_URL = `https://restcountries.eu/rest/v2`;
     
-// };
+
+function fetchCountries(searchQuery) {
+    return fetch(`${BASE_URL}/name/${searchQuery}`)
+        .then(response => response.json()); 
+}
+
+export default { fetchCountries };
